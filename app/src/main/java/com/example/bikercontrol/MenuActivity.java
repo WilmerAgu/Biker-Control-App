acef2f6e4c0061be2cb26933cadd73adcb7d92a2
WilmerAgu <105666923+wilmeragudelo@users.noreply.github.com>
2024-12-09 12:33:46 -0500

    creando layout inici, menu, regitro de partes

---
+++ app/src/main/java/com/example/bikercontrol/MenuActivity.java
@@ -0,0 +1,39 @@
+package com.example.bikercontrol;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuActivity extends AppCompatActivity {
+    private Button btnRegisterParts;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnRegisterParts = findViewById(R.id.btnRegisterParts);
+
+        btnRegisterParts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MenuActivity.this, RegisterPartsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
